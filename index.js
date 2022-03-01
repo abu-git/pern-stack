6357614dab791564874c2e856da55accd6ee9e34
abu-git <abu.sammie@gmail.com>
2022-03-01 16:48:36 +0200

    CRUD methods started

--- index.js
+++ index.js
@@ -28,10 +28,27 @@ app.post("/todos", async(req, res) => {
 })
 
 //get all todos
-
+app.get("/todos", async(req, res) => {
+    try{
+        const allTodos = await pool.query("SELECT * FROM todo")
+        res.json(allTodos.rows)
+    }catch(err){
+        console.error(err.message)
+    }
+})
 
 //get a todo
+app.get("/todos/:id", async(req, res) => {
+    try{
+        //console.log(req.params)
+        const { id } = req.params
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
 
+        res.json(todo.rows[0])
+    }catch(err){
+        console.error(err.message) 
+    }
+})
 
 //update a todo
 
