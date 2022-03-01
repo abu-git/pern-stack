a03a8fcc042aeae46ecb8fba0c98e8fa50d6e073
abu-git <abu.sammie@gmail.com>
2022-03-01 22:17:55 +0200

    CRUD methods working

--- index.js
+++ index.js
@@ -67,6 +67,18 @@ app.put("/todos/:id", async(req, res) => {
 })
 
 //delete a todo
+app.delete("/todos/:id", async(req, res) => {
+    try{
+        const { id } = req.params
+        const deleteTodo = await pool.query(
+            "DELETE FROM todo WHERE todo_id = $1",
+            [id]
+        )
+        res.json("Todo was deleted")
+    }catch(err){
+        console.error(err.message)
+    }
+})
 
 app.listen(5000, ()=>{
     console.log("Server started on port 5000")
